ea05b2325b77a7a4d64fc5a21f6c2af10ec44cf7
KaHoMRGT <58539787+KaHoMRGT@users.noreply.github.com>
2019-12-12 11:28:04 +0900

    Create school_time.js

---
+++ school_time.js
@@ -0,0 +1,134 @@
+
+
+function select(){
+  var div = document.getElementById('shirabasu');
+  div.style.visibility = 'visible';
+
+}
+
+document.getElementById('Mon1').addEventListener('click', function() {
+  var div = document.getElementById('shirabasu');
+  div.style.visibility = 'visible';
+
+  document.getElementById('Mon1').innerHTML = "math1";
+}, false);
+document.getElementById('Tue1').addEventListener('click', function() {
+  var div = document.getElementById('shirabasu');
+  div.style.visibility = 'visible';
+  document.getElementById('Tue1').innerHTML = "math2";
+}, false);
+document.getElementById('Wed1').addEventListener('click', function() {
+  var div = document.getElementById('shirabasu');
+  div.style.visibility = 'visible';
+  document.getElementById('Wed1').innerHTML = "math3";
+}, false);
+document.getElementById('Thu1').addEventListener('click', function() {
+  var div = document.getElementById('shirabasu');
+  div.style.visibility = 'visible';
+  document.getElementById('Thu1').innerHTML = "math4";
+}, false);
+document.getElementById('Fri1').addEventListener('click', function() {
+  var div = document.getElementById('shirabasu');
+  div.style.visibility = 'visible';
+  document.getElementById('Fri1').innerHTML = "math5";
+}, false);
+document.getElementById('Mon2').addEventListener('click', function() {
+  var div = document.getElementById('shirabasu');
+  div.style.visibility = 'visible';
+  document.getElementById('Mon2').innerHTML = "math1";
+}, false);
+document.getElementById('Tue2').addEventListener('click', function() {
+  var div = document.getElementById('shirabasu');
+  div.style.visibility = 'visible';
+  document.getElementById('Tue2').innerHTML = "math1";
+}, false);
+document.getElementById('Wed2').addEventListener('click', function() {
+  var div = document.getElementById('shirabasu');
+  div.style.visibility = 'visible';
+  document.getElementById('Wed2').innerHTML = "math3";
+}, false);
+document.getElementById('Thu2').addEventListener('click', function() {
+  var div = document.getElementById('shirabasu');
+  div.style.visibility = 'visible';
+  document.getElementById('Thu2').innerHTML = "math4";
+}, false);
+document.getElementById('Fri2').addEventListener('click', function() {
+  var div = document.getElementById('shirabasu');
+  div.style.visibility = 'visible';
+  document.getElementById('Fri2').innerHTML = "math5";
+}, false);
+document.getElementById('Mon3').addEventListener('click', function() {
+  var div = document.getElementById('shirabasu');
+  div.style.visibility = 'visible';
+  document.getElementById('Mon3').innerHTML = "math1";
+}, false);
+document.getElementById('Tue3').addEventListener('click', function() {
+  var div = document.getElementById('shirabasu');
+  div.style.visibility = 'visible';
+  document.getElementById('Tue3').innerHTML = "math1";
+}, false);
+document.getElementById('Wed3').addEventListener('click', function() {
+  var div = document.getElementById('shirabasu');
+  div.style.visibility = 'visible';
+  document.getElementById('Wed3').innerHTML = "math3";
+}, false);
+document.getElementById('Thu3').addEventListener('click', function() {
+  var div = document.getElementById('shirabasu');
+  div.style.visibility = 'visible';
+  document.getElementById('Thu3').innerHTML = "math4";
+}, false);
+document.getElementById('Fri3').addEventListener('click', function() {
+  var div = document.getElementById('shirabasu');
+  div.style.visibility = 'visible';
+  document.getElementById('Fri3').innerHTML = "math5";
+}, false);
+document.getElementById('Mon4').addEventListener('click', function() {
+  var div = document.getElementById('shirabasu');
+  div.style.visibility = 'visible';
+  document.getElementById('Mon4').innerHTML = "math1";
+}, false);
+document.getElementById('Tue4').addEventListener('click', function() {
+  var div = document.getElementById('shirabasu');
+  div.style.visibility = 'visible';
+  document.getElementById('Tue4').innerHTML = "math1";
+}, false);
+document.getElementById('Wed4').addEventListener('click', function() {
+  var div = document.getElementById('shirabasu');
+  div.style.visibility = 'visible';
+  document.getElementById('Wed4').innerHTML = "math3";
+}, false);
+document.getElementById('Thu4').addEventListener('click', function() {
+  var div = document.getElementById('shirabasu');
+  div.style.visibility = 'visible';
+  document.getElementById('Thu4').innerHTML = "math4";
+}, false);
+document.getElementById('Fri4').addEventListener('click', function() {
+  var div = document.getElementById('shirabasu');
+  div.style.visibility = 'visible';
+  document.getElementById('Fri4').innerHTML = "math5";
+}, false);
+document.getElementById('Mon5').addEventListener('click', function() {
+  var div = document.getElementById('shirabasu');
+  div.style.visibility = 'visible';
+  document.getElementById('Mon5').innerHTML = "math1";
+}, false);
+document.getElementById('Tue5').addEventListener('click', function() {
+  var div = document.getElementById('shirabasu');
+  div.style.visibility = 'visible';
+  document.getElementById('Tue5').innerHTML = "math1";
+}, false);
+document.getElementById('Wed5').addEventListener('click', function() {
+  var div = document.getElementById('shirabasu');
+  div.style.visibility = 'visible';
+  document.getElementById('Wed5').innerHTML = "math3";
+}, false);
+document.getElementById('Thu5').addEventListener('click', function() {
+  var div = document.getElementById('shirabasu');
+  div.style.visibility = 'visible';
+  document.getElementById('Thu5').innerHTML = "math4";
+}, false);
+document.getElementById('Fri5').addEventListener('click', function() {
+  var div = document.getElementById('shirabasu');
+  div.style.visibility = 'visible';
+  document.getElementById('Fri5').innerHTML = "math5";
+}, false);
