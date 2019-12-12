a788b963b466f0f040e59735b5fa4d820ff50b01
KaHoMRGT <58539787+KaHoMRGT@users.noreply.github.com>
2019-12-12 11:25:02 +0900

    Create portal.js

---
+++ portal.js
@@ -0,0 +1,15 @@
+$(function() {
+  $('.ChangeElem_Panel').hide();
+$('.ChangeElem_Panel').eq(0).show();
+$('.ChangeElem_Btn').eq(0).addClass('is-active');
+/*クリックイベント*/
+$('.ChangeElem_Btn').each(function () {
+  $(this).on('click', function () {
+    var index = $('.ChangeElem_Btn').index(this);
+    $('.ChangeElem_Btn').removeClass('is-active');
+    $(this).addClass('is-active');
+    $('.ChangeElem_Panel').hide();
+    $('.ChangeElem_Panel').eq(index).show();
+  });
+});
+});
